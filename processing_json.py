72fdbf7b4fb265ca98d99e317c353858708c1f15
Anna Luiza Caraciolo <alcaf@cin.ufpe.br>
2024-04-17 19:22:20 -0300

    Adding all code llama notebooks

--- processing_json.py
+++ processing_json.py
@@ -1,10 +1,17 @@
 import csv
 import json
-
-with open('MojoDataset-TestFormatado.csv', newline='') as csvfile:
+# generate train
+with open('data/MojoDataset-TrainFormatado.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     data = [row for row in reader]
 
+with open('data/MojoTrain-Formatado.json', 'w') as jsonfile:
+    json.dump(data, jsonfile)
+    
+# generate test
+with open('data/MojoDataset-TestFormatado.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    data = [row for row in reader]
 
-with open('MojoTest-Formatado.json', 'w') as jsonfile:
+with open('data/MojoTest-Formatado.json', 'w') as jsonfile:
     json.dump(data, jsonfile)
