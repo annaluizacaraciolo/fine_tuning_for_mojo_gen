1f6071bdec31747a28ff9b7f47ac5e58744d03dc
Anna Luiza Caraciolo <alcaf@cin.ufpe.br>
2024-06-06 20:39:07 -0300

    finished infilling model and dataset

--- generate_json_infilling.py
+++ generate_json_infilling.py
@@ -1,27 +1,6 @@
 import json
 import random
-
-def split_text_randomly_and_write(file_path, output_path):
-    with open(file_path, 'r') as file:
-        data = json.load(file)
-        
-    results = []
-    
-    for item in data:
-        text = item["text"]
-        if len(text) > 2:  # Ensure there's enough length to split into three parts
-            split_points = sorted(random.sample(range(1, len(text)), 2))
-            part1 = text[:split_points[0]]
-            part2 = text[split_points[0]:split_points[1]]
-            part3 = text[split_points[1]:]
-            # prefix-suffix-middle format
-            results.append({"text": "<PRE>" + part1 + "<SUF>" + part3 + "<MID>" + part2})
-    
-    with open(output_path, 'w') as outfile:
-        json.dump(results, outfile, indent=4)
-    
-    return output_path
-
+import csv
 
 def split_user_input_randomly(file_path, output_path):
     with open(file_path, 'r') as file:
@@ -35,23 +14,28 @@ def split_user_input_randomly(file_path, output_path):
             split_points = sorted(random.sample(range(1, len(text)), 2))
             part1 = text[:split_points[0]]
             part2 = text[split_points[1]:]
-            # prefix-suffix-middle format
-            results.append({"user_input": "<PRE>" + part1 + "<SUF>" + part2 + "<MID>", "system_answer": item["system_answer"]})
+            results.append({"user_input": part1 + "<FILL_ME>" + part2, "system_answer": item["system_answer"]})
     
     with open(output_path, 'w') as outfile:
         json.dump(results, outfile, indent=4)
     
     return output_path
 
-# Generate train
-file_path = 'data/code_completion/CodeCompletion-Train.json'  
-output_path = 'data/infilling/Infilling-Train.json' 
-result_file_path = split_text_randomly_and_write(file_path, output_path)
+# generate train
+with open('data/code_completion/CodeCompletion-Train.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    data = [row for row in reader]
 
-# Generate validation
-file_path = 'data/code_completion/CodeCompletion-Validation.json'  
-output_path = 'data/infilling/Infilling-Validation.json' 
-result_file_path = split_text_randomly_and_write(file_path, output_path)
+with open('data/infilling/Infilling-Train.json', 'w') as jsonfile:
+    json.dump(data, jsonfile)
+    
+# generate validation
+with open('data/code_completion/CodeCompletion-Validation.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    data = [row for row in reader]
+
+with open('data/infilling/Infilling-Validation.json', 'w') as jsonfile:
+    json.dump(data, jsonfile)
 
 # Generate test
 file_path = 'data/code_completion/CodeCompletion-Test.json'  
